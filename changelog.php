3f9d145074f1d13f8e9d062f61ceb81f5ad4e143
Plopkoekk <41813101+Plopkoekk@users.noreply.github.com>
2024-11-04 23:25:31 +0100

    test

--- changelog.php
+++ changelog.php
@@ -6,7 +6,7 @@
     <hr class="md-3">
     <h4>Changelog:</h4>
     <ul>
-        <li>TEST ETST ESTESTES TSETSETSET ESTSE T SET SE TSE T SET ESTE TEST 2024/11/04 - Added automated deploy with git and webhook for ease of updating.</li>
+        <li>TEST1 2024/11/04 - Added automated deploy with git and webhook for ease of updating.</li>
         <li>2024/11/04 - Initial push to git for version management</li>
         <li>2024/11/03 - inHome Install, and unhappy drop forms migrated to new design with input-groups. Rest to follow.</li>
         <li>2024/11/03 - Migrated CSS from custom-written to Bootstrap 5.3 for coherent design and ease-of-maintenace. Forms still to be migrated fully.</li>
