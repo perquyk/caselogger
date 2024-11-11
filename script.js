88280a6d823a6d86c73e1222ae80b6462b31a372
Plopkoekk <perquy.k@gmail.com>
2024-11-11 17:41:42 +0100

    small bug in variable declaration in generateCFNCaselog()

--- script.js
+++ script.js
@@ -448,7 +448,7 @@ function generateCFNCaselog(){
         catsap: document.getElementById('testen_catsap').value,
         filter: document.getElementById('testen_filterstatus').value,
         streetping: document.getElementById('testen_streetping').value,
-        acties: document.getElementById('testen_acties').value,
+        acties: document.getElementById('acties').value,
         router: document.getElementById('router').value,
         niu: document.getElementById('niu').value,
         modemtype: document.getElementById('modemtype').value,
