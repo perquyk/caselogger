a1699ba38d005e260c39bb0a4fd2adbeccc806f6
Plopkoekk <perquy.k@gmail.com>
2024-12-09 00:20:41 +0100

    changelog updated

--- stores/changelogStore.js
+++ stores/changelogStore.js
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia'
 export const useChangeStore = defineStore('changeStore', {
     state: () => ({
         changes: [
+            { date: '8/12/2024', description: 'Added repair form' },
             {
                 date: '8/12/2024',
                 description: 'Added changelog and basic structure to homepage',
