a2a66f954ac527d5b4845590f0d3286189b1ee0c
Kenny Perquy <perquy.k@gmail.com>
2024-12-11 00:47:36 +0100

    CSL-65 added temp link to old.perquyk.org

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -71,7 +71,7 @@ const inhomeLinks = [
 const ftthLinks = [[{ label: 'RFoG Install', to: '/forms/ftth/rfog-install' }]]
 const b2bLinks = [
     [
-        { label: 'CFN', to: '/forms/b2b/cfn' },
+        { label: 'CFN (OLD)', to: 'https://old.perquyk.org/' },
         { label: 'PSV KA', to: '/forms/b2b/psv-ka' },
     ],
 ]
@@ -92,7 +92,7 @@ const allLinks = [
     ],
     [
         { label: 'B2B', disabled: true },
-        { label: 'CFN', to: '/forms/b2b/cfn' },
+        { label: 'CFN (OLD)', to: 'https://old.perquyk.org/' },
         { label: 'PSV KA', to: '/forms/b2b/psv-ka' },
     ],
     [
