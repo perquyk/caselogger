a924dc4ddfc45a6ae23ae9cc9430e8ad804d54da
Kenny Perquy <perquy.k@gmail.com>
2024-12-10 13:42:40 +0100

    CSL-12 #comment Made skeleton for the gsx form

--- pages/forms/b2b/projects/gsx.vue
+++ pages/forms/b2b/projects/gsx.vue
@@ -1,7 +1,47 @@
 <template>
-    <div>test</div>
+    <div class="mx-auto grid w-9/12 grid-cols-12 gap-3 p-3">
+        <FormDiv title="GSX Migration" />
+        <FormDiv header="Woli Info" cols="12">
+            <FormModuleWoliInfo />
+        </FormDiv>
+        <FormDiv header="Installatie Info">install info</FormDiv>
+        <FormDiv header="Testen">testen</FormDiv>
+        <FormDiv header="Finishing Up">finishing up</FormDiv>
+        <FormDiv>
+            <FormModuleButtonGroup
+                :formData="formData"
+                :task="useFormStore.task"
+                taskType="GSX-Migration"
+            />
+        </FormDiv>
+    </div>
 </template>
 
-<script lang="ts" setup></script>
+<script setup>
+const formStore = useFormStore()
+const formData = () => {
+    return `Taaktype: Project GSX-Migration
+WOLI:
+Adres:
+------------------------------
+Omschrijving Taak:
+Project GSX Migratie
+- Aanwezige IAD-type: 
 
-<style></style>
+Testen:
+Testcalls Argus/Phonerlite:
+Testcalls PaBX:
+
+Issues/Remarks:
+
+
+Materiaal:
+1x Marakele - 
+1x Audiocode - 
+
+Resultaat:
+
+
+Modemtest:`
+}
+</script>
