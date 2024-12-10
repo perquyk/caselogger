27ad1694a5540f70c58825d11ffbd174c781fa7f
Kenny Perquy <perquy.k@gmail.com>
2024-12-10 14:38:56 +0100

    CSL-16 #comment removed WIP message

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,6 @@
     <MainContainer title="Caselogger" logo="Logo32.png">
         <ContainerSection>
             <p>
-                inhome install en repair forms zijn klaar. rest is nog altijd
-                WIP :)
-            </p>
-            <br />
-            <p>
                 Kies Bovenaan de gewenste template.<br />
                 Vul daarna de template in en druk op copy of download.<br />
                 Done.<br />
