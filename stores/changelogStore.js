7ad088035ae331dff8e803b473e8210c417a52e2
Kenny Perquy <perquy.k@gmail.com>
2024-12-11 00:19:43 +0100

    CSL-62 edited changelog

--- stores/changelogStore.js
+++ stores/changelogStore.js
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia'
 export const useChangeStore = defineStore('changeStore', {
     state: () => ({
         changes: [
+            { data: '11/12/2024', description: 'Added PSV KA Form' },
             { date: '10/12/2024', description: 'Added RFoG and GSX Forms' },
             {
                 date: '9/12/2024',
