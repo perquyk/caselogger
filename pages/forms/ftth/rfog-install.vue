9dcc05e68e61d5107f8d94946fc28dfe1f481790
Kenny Perquy <perquy.k@gmail.com>
2024-12-10 12:01:31 +0100

    CSL-7 #comment added general store for all select options
    Made ticket (CSL-9) to migrate all other stores that store select options.

--- pages/forms/ftth/rfog-install.vue
+++ pages/forms/ftth/rfog-install.vue
@@ -1,7 +1,55 @@
 <template>
-    <div>test</div>
+    <div class="mx-auto grid w-9/12 grid-cols-12 gap-3 p-3">
+        <FormDiv title="RFoG Install" />
+        <FormDiv header="Klant Info">
+            <FormModuleKlantInfo />
+        </FormDiv>
+        <FormDiv header="Situatie bij aankomst" cols="12">
+            <FormModuleInstallSituatie />
+        </FormDiv>
+        <FormDiv header="Acties" cols="12">
+            <FormModuleInhomeActies />
+        </FormDiv>
+        <FormDiv header="Finishing up">
+            <FormModuleFinish />
+        </FormDiv>
+        <FormDiv header="">
+            <FormModuleButtonGroup
+                :formData="formData"
+                :task="formStore.task"
+                taskType="install"
+            />
+        </FormDiv>
+    </div>
 </template>
 
-<script lang="ts" setup></script>
+<script setup>
+const formStore = useFormStore()
+const formData = () => {
+    return `Taaktype: inHome Install
+TaakID: ${formStore.task}
+Klantnummer: ${formStore.klantnummer}
+Adres: ${formStore.adres}
+------------------------------
+Omschrijving Taak:
+${formStore.omschrijving}
 
-<style></style>
+Testen bij aankomst:
+Catsap: ${formStore.catsap}
+Filter: ${formStore.filter}
+Streetping: ${formStore.streetping}
+
+Acties:
+${formStore.acties}
+
+Materiaal:
+${formStore.materiaal}
+
+Resultaat:
+${formStore.resultaat}
+
+Modemtest:
+${formStore.modemtest}
+`
+}
+</script>
