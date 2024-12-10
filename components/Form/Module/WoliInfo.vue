63167c7c695a3d25935f96f89c38253f3a9249ab
Kenny Perquy <perquy.k@gmail.com>
2024-12-10 13:42:14 +0100

    CSL-12 Added Woli info component

---
+++ components/Form/Module/WoliInfo.vue
@@ -0,0 +1,10 @@
+<template>
+    <FormInput label="WOLI" class="col-span-6" v-model="formStore.task" />
+    <FormInput label="Adres" class="col-span-6" v-model="formStore.adres" />
+</template>
+
+<script setup>
+const formStore = useFormStore()
+</script>
+
+<style></style>
