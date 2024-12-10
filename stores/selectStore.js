0ec6c2e357392bcda73f1bc5785cf6e4caa8f292
Kenny Perquy <perquy.k@gmail.com>
2024-12-10 12:08:00 +0100

    CSL7 modified selectStore

--- stores/selectStore.js
+++ stores/selectStore.js
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia'
 export const useFilterStore = defineStore('filterStore', {
     state: () => ({
         patchLengthOptions: [
+            { value: 'nvt', description: 'nvt' },
             { value: 'SC-LC 2m', description: 'SC-LC 2m' },
             { value: 'SC-LC 3m', description: 'SC-LC 3m' },
             { value: 'SC-SC 2m', description: 'SC-SC 2m' },
