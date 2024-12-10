ecd0facce44ad32cd8f44e00fad281bd9344f76c
Kenny Perquy <perquy.k@gmail.com>
2024-12-10 14:01:54 +0100

    CSL-12 Added necessary values in selectStore.js

--- stores/selectStore.js
+++ stores/selectStore.js
@@ -8,5 +8,19 @@ export const useSelectStore = defineStore('selectStore', {
             { value: 'SC-SC 2m', description: 'SC-SC 2m' },
             { value: 'SC-SC 1m', description: 'SC-SC 1m' },
         ],
+        gsxInstallType: [
+            { value: 'SIP', description: 'SIP' },
+            { value: 'ISDN', description: 'ISDN' },
+            { value: 'ISDN-BRI', description: 'ISDN-BRI' },
+            { value: 'PRA', description: 'PRA' },
+        ],
+        gsxOldsetup: [
+            { value: 'oneAccess', description: 'oneAccess' },
+            { value: 'AudioCodes', description: 'AudioCodes' },
+        ],
+        oknok: [
+            { value: 'OK', description: 'OK' },
+            { value: 'NOK', desription: 'NOK' },
+        ],
     }),
 })
