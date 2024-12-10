a5a4dddc450c25a5840c7ea74bc6958f6b4127c9
Plopkoekk <perquy.k@gmail.com>
2024-12-10 22:58:11 +0100

    WIP on master

--- stores/selectStore.js
+++ stores/selectStore.js
@@ -1,6 +1,21 @@
 import { defineStore } from 'pinia'
 export const useSelectStore = defineStore('selectStore', {
     state: () => ({
+        catsapOptions: [
+            { value: 1, description: '1 - Active Cable Customer' },
+            { value: 2, description: '2 - Former Cable Customer' },
+            { value: 3, description: '3 - No Dropcable (cable in street)' },
+            { value: 4, description: '4 - No Service (No cable in street)' },
+            { value: 5, description: '5 - Rijgnet' },
+            {
+                value: 6,
+                description: '6 - Dropcable Finished (Ready for connection',
+            },
+            {
+                value: 7,
+                description: '7 - Dropcable Placed (Not ready for connection)',
+            },
+        ],
         patchLengthOptions: [
             { value: '', description: '' },
             { value: 'nvt', description: 'nvt' },
