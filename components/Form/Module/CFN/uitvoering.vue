0dd9f8cc04dc02b0642d60a3e94d482567a5add2
Kenny Perquy <perquy.k@gmail.com>
2024-12-15 00:45:56 +0100

    CSL-78 made uitvoering2 uitvoering1

--- components/Form/Module/CFN/uitvoering.vue
+++ components/Form/Module/CFN/uitvoering.vue
@@ -5,65 +5,58 @@
         v-model="formStore.acties"
     />
     <FormSelect
-        class="col-span-4"
-        label="Modem Type"
-        v-model="formStore.modemtype"
-    />
-    <FormInput class="col-span-4" :label="MacLabel()" v-model="formStore.hfc" />
-
-    <FormSelect
-        class="col-span-4"
-        label="NIU"
-        v-model="formStore.niu"
-        :options="selectStore.niu"
-    />
-    <FormSelect
+        v-if="formStore.managed == 'Managed'"
         class="col-span-12"
         label="Router"
         v-model="formStore.router"
         :options="selectStore.router"
-        v-if="showRouter()"
+    />
+    <FormSelect
+        v-if="
+            formStore.main == 'VDSL' ||
+            formStore.main == 'Telenet Coax' ||
+            formStore.main == 'Mobile'
+        "
+        class="col-span-4"
+        label="Modem Type"
+        v-model="formStore.modemtype"
+        :options="
+            formStore.main == 'VDSL'
+                ? selectStore.modemtype.vdsl
+                : formStore.main == 'Telenet Coax'
+                  ? selectStore.modemtype.tlnCoax
+                  : formStore.main == 'Mobile'
+                    ? selectStore.modemtype.mob
+                    : ''
+        "
+    />
+    <FormInput
+        class="col-span-4"
+        v-if="
+            formStore.main == 'Telenet Coax' ||
+            formStore.main == 'VDSL' ||
+            formStore.main == 'GPON' ||
+            (formStore.main == 'Mobile' && formStore.modemtype != 'Coiler/NGIS')
+        "
+        :label="
+            formStore.main == 'Telenet Coax'
+                ? 'HFC MAC'
+                : formStore.main == 'Mobile'
+                  ? 'IMEI'
+                  : 'CID'
+        "
+        v-model="formStore.hfc"
+    />
+    <FormSelect
+        class="col-span-4"
+        v-if="formStore.main == 'Telenet Coax'"
+        v-model="formStore.niu"
+        :options="selectStore.niu"
+        label="NIU"
     />
 </template>
 
 <script setup>
 const formStore = useFormStore()
 const selectStore = useSelectStore()
-const showRouter = () => {
-    if (formStore.managed === 'Managed') {
-        return true
-    } else {
-        return false
-    }
-}
-let modemType = () => {
-    switch (formStore.main) {
-        case 'Telenet Coax':
-            return selectStore.modemtype.tlnCoax
-        case 'VDSL':
-            return selectStore.modemtype.vdsl
-        case 'gpon':
-            return selectStore.modemtype.gpon
-        case 'VOO Coax':
-            return selectStore.modemtype.vooCoax
-        case 'Mobile':
-            return selectStore.modemtype.mob
-    }
-}
-const MacLabel = () => {
-    switch (formStore.modemtype) {
-        case 'D3.1 ERT':
-            return 'HFC-MAC'
-        case 'AMOD 3':
-        case 'AMOD 4':
-            return 'CID'
-        case 'ZTE':
-        case 'Nokia 5G':
-            return 'IMEI'
-        case 'Coiler/NGIS':
-            return ''
-        default:
-            return 'SN'
-    }
-}
 </script>
