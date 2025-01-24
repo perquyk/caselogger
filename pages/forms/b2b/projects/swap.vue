2738c9479a39bbfc97ab038ca8e1223773638c95
Kenny Perquy <perquy.k@gmail.com>
2025-01-24 21:49:17 +0100

    Added the output script for swap project

--- pages/forms/b2b/projects/swap.vue
+++ pages/forms/b2b/projects/swap.vue
@@ -13,8 +13,89 @@
         <FormDiv header="Finishing up">
             <FormModuleWoliFinish/>
         </FormDiv>
+        <FormDiv>
+            <FormModuleButtonGroup
+                :formData="formData"
+                :task="formStore.task"
+                taskType="ProjSwap"
+            />
+        </FormDiv>
     </div>
 </template>
 <script setup>
+    const formStore = useFormStore();
+    const formData = () => {
+        return `Taaktype: ${taskType()}
+WOLI: ${formStore.task}
+Adres: ${formStore.adres}
+------------------------------
+${situatie()}
 
+${acties()}
+
+${materiaal()}
+
+Remark/Issues:
+${formStore.remarks}
+
+Resultaat:
+${formStore.resultaat}
+
+Modemtest:
+${formStore.modemtest}
+`
+    }
+    const taskType = () => {
+        if(formStore.modemSwapCheck && !formStore.niuSwapCheck){
+            return "Project Modem D3.1 Swap"
+        }
+        if(!formStore.modemSwapCheck && formStore.niuSwapCheck){
+            return "Project 85MHz NIU Swap"
+        }
+        if(formStore.modemSwapCheck && formStore.niuSwapCheck){
+            return "Project Modem D3.1 Swap + 85MHz NIU Swap"
+        }
+    }
+    const situatie = () => {
+        let output = `Omschrijving taak:`;
+        if(formStore.modemSwapCheck){
+            output += `
+  - Modem swap naar D3.1`;
+            if(formStore.wirelessCheck){
+                output += ` Wireless (op vraag van WOLI)`
+            }
+            if(formStore.bridgeCheck){
+                output += ` -> Mac-bridging nodig`
+            }
+        }
+        if(formStore.niuSwapCheck){
+            output += `
+  - NIU`
+        }
+        return output;
+    };
+    const acties = () => {
+        let output = `Acties:`;
+        if(formStore.modemSwapped){
+            output += `
+  - Modem geswapt naar ${formStore.modemtype} (${formStore.hfc})`
+        }
+        if(formStore.niuSwapped){
+            output += `
+  - NIU geswapt naar ${formStore.niu}`
+        }
+        return output;
+    };
+    const materiaal = () => {
+        let output = `Materiaal:`;
+        if(formStore.modemtype){
+            output += `
+  - 1x ${formStore.modemtype} - ${formStore.hfc}`
+        }
+        if(formStore.niu){
+            output += `
+  - 1x ${formStore.niu}`
+        }
+        return output;
+    };
 </script>
